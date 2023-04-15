95cb48671177906b29d959ff9bfaa4068a022d94
Alexandr Gorbunov <asin.gorbunov@yandex.ru>
2023-04-15 10:35:11 +0300

    Initial commit

---
+++ WebBooks/WebBooks/urls.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.urls import path, include
+from catalog import views
+# from WebBooks.catalog import views
+# from WebBooks.catalog import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('admin/', admin.site.urls),
+    path(r'^books/$', views.BookListView.as_view(), name='books'),
+    path(r'^book/(?P<pk>\d+)$', views.BookDetailView.as_view(), name='book-detail'),
+    path(r'author/', views.AuthorListView.as_view(), name='authors'),
+    path('accounts/', include('django.contrib.auth.urls')),
+]
